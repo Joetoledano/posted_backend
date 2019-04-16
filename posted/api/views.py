b4d2ac76d408bb81333209e0f4ec96d93228c91c
Saahith Pochiraju <saahith116@gmail.com>
2019-04-15 21:40:22 -0400

    pruning

--- posted/api/views.py
+++ posted/api/views.py
@@ -1,18 +1,8 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from posted.api.serializers import UserSerializer, GroupSerializer
 from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.response import Response
-from util.db_util import get_deals
+from util.db_util import get_deals, get_category_deals
 
-
-
-@api_view(["GET"])
-@authentication_classes([])
-def userview(request):
-    queryset = User.objects.all().order_by('-date_joined')
-    serialized = UserSerializer(queryset, many=True)
-    return Response({'data':serialized.data})
 
 @api_view(["GET"])
 @authentication_classes([])
@@ -22,11 +12,13 @@ def deals(request):
 
 @api_view(["GET"])
 @authentication_classes([])
-def groupview(request):
-    queryset = Group.objects.all()
-    serialized =  GroupSerializer(queryset, many=True)
-    return Response({'data':serialized.data})
-
+def categories(request):
+    primary = request.GET.get('primary')
+    if not primary:
+        return Response({'msg':'no primary'})
+    secondary = request.GET.get('secondary')
+    deals = get_category_deals(primary, secondary)
+    return Response(deals)
 
 @api_view(["GET"])
 @authentication_classes([])
--- util/db_util.py
+++ util/db_util.py
@@ -13,3 +13,18 @@ def get_deals():
         cursor.execute(sql_statement)
         db.close()
         return cursor.fetchall()
+
+
+def get_category_deals(primary, secondary):
+    try:
+        db = pymysql.connect(host=HOSTNAME, user=USERNAME, password=PASSWORD, db=DB, cursorclass=pymysql.cursors.DictCursor)
+        cursor = db.cursor()
+    except Exception as e:
+        print(e)
+    else:
+        secondary = " and secondary_category ='{}'".format(secondary) if secondary else ''
+        sql_statement = """ select * from deals where primary_category = '{}' {}""".format(primary, secondary)
+        print(sql_statement)
+        cursor.execute(sql_statement)
+        db.close()
+        return cursor.fetchall()
